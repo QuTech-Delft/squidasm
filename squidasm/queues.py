cb93cb71d3d412ce22372f7566cfce19e0d24047
Bart van der Vecht <b.vandervecht@tudelft.nl>
2020-05-27 09:25:19 +0200

    Connections now wait a little for a queue that does not yet exist

--- squidasm/queues.py
+++ squidasm/queues.py
@@ -1,11 +1,14 @@
 from queue import Queue
+from time import sleep
+from timeit import default_timer as timer
 
 from netqasm.messages import Signal
 
 _QUEUES = {}
 
 
-def get_queue(node_name, key=None, create_new=False):
+def get_queue(node_name, key=None, create_new=False, wait_for=5.0):
+    """ wait_for: time in secs to wait for queue to be created if not exists """
     absolute_key = (node_name, key)
     queue = _QUEUES.get(absolute_key)
     if queue is None:
@@ -13,8 +16,22 @@ def get_queue(node_name, key=None, create_new=False):
             queue = Queue()
             _QUEUES[absolute_key] = queue
         else:
-            raise RuntimeError(f"Trying to get queue with name {node_name}, but it doesn't exist.")
+            queue = wait_for_queue_creation(absolute_key, wait_for)
     return queue
+
+
+def wait_for_queue_creation(absolute_key, timeout=5.0):
+    t_start = timer()
+
+    while True:
+        queue = _QUEUES.get(absolute_key)
+        if queue is not None:
+            return queue
+
+        sleep(0.1)
+        now = timer()
+        if (now - t_start) > timeout:
+            raise TimeoutError(f"No queue found with key {absolute_key}. (Waited for {timeout} seconds)")
 
 
 def signal_queue(node_name, signal, key=None):
