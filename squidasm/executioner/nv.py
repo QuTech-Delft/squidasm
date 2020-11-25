af928e90f2b4b8ae364e9d93ecf5cd32a1e52d1e
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-11-25 17:11:07 +0100

    Only allow measure in the electron for nv

--- squidasm/executioner/nv.py
+++ squidasm/executioner/nv.py
@@ -25,3 +25,11 @@ class NVNetSquidExecutioner(NetSquidExecutioner):
     def __init__(self, node, name=None, network_stack=None, instr_log_dir=None, flavour=None):
         """Represents a QNodeOS processor that communicates with a QDevice that supports NV instructions"""
         super().__init__(node, name, network_stack, instr_log_dir, instr_mapping=NV_NS_INSTR_MAPPING)
+
+    def _do_meas(self, subroutine_id, q_address):
+        position = self._get_position(subroutine_id=subroutine_id, address=q_address)
+        assert position == 0, "Can only measure the electron directly"
+        super()._do_meas(
+            subroutine_id=subroutine_id,
+            q_address=q_address,
+        )
