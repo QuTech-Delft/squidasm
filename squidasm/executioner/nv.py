cdbf8b84034336dd1315918ca17ab931aaa2e44f
Bart van der Vecht <b.vandervecht@tudelft.nl>
2020-11-25 19:16:59 +0100

    Do not use super() method

--- squidasm/executioner/nv.py
+++ squidasm/executioner/nv.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from pydynaa import EventExpression
+
 from netsquid.components.instructions import (
     INSTR_INIT,
     INSTR_ROT_X,
@@ -32,15 +34,23 @@ class NVNetSquidExecutioner(NetSquidExecutioner):
         position = self._get_position(subroutine_id=subroutine_id, address=q_address)
         if position != 0:  # a carbon
             # Move the state to the electron (position=0) first and then measure the electron.
+            # See https://gitlab.tudelft.nl/qinc-wehner/netqasm/netqasm-docs/-/blob/master/nv-gates-docs.md
+            # for the circuit.
             self._logger.debug(f"Moving qubit from carbon (position {position}) to electron before measuring")
+            yield from self._execute_qdevice_instruction(ns_instr=INSTR_INIT, qubit_mapping=[0])
             yield from self._execute_qdevice_instruction(ns_instr=INSTR_ROT_Y, qubit_mapping=[0], angle=np.pi/2)
-            yield from self._execute_qdevice_instruction(ns_instr=INSTR_CYDIR, qubit_mapping=[0, position], angle=-np.pi/2)
+            yield from self._execute_qdevice_instruction(
+                ns_instr=INSTR_CYDIR, qubit_mapping=[0, position], angle=-np.pi/2)
             yield from self._execute_qdevice_instruction(ns_instr=INSTR_ROT_X, qubit_mapping=[0], angle=-np.pi/2)
-            yield from self._execute_qdevice_instruction(ns_instr=INSTR_CXDIR, qubit_mapping=[0, position], angle=np.pi/2)
+            yield from self._execute_qdevice_instruction(
+                ns_instr=INSTR_CXDIR, qubit_mapping=[0, position], angle=np.pi/2)
             yield from self._execute_qdevice_instruction(ns_instr=INSTR_ROT_Y, qubit_mapping=[0], angle=-np.pi/2)
 
         # Measure the electron.
-        yield super()._do_meas(
-            subroutine_id=subroutine_id,
-            q_address=0,
-        )
+        # NOTE: we cannot use super()._do_meas() since it will try to find the electron in the unit module,
+        # but it was already freed and hence not in the unit module.
+        self._logger.debug(f"Measuring qubit {position}")
+        if self.qdevice.busy:
+            yield EventExpression(source=self.qdevice, event_type=self.qdevice.evtype_program_done)
+        outcome = self.qdevice.measure(position)[0][0]
+        return outcome
