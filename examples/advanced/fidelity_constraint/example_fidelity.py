035fcb1f3d22276bedf5768aed88c42f22fc4874
mvanhooft <m.k.vanhooft@tudelft.nl>
2025-12-18 14:33:56 +0100

    Revert example_fidelity.py to original

--- examples/advanced/fidelity_constraint/example_fidelity.py
+++ examples/advanced/fidelity_constraint/example_fidelity.py
@@ -5,9 +5,6 @@ import os
 from typing import Any, Dict, Generator
 
 import netsquid as ns
-from netqasm.sdk.connection import BaseNetQASMConnection
-from netqasm.sdk.futures import RegFuture
-from netqasm.sdk.qubit import Qubit
 
 from pydynaa import EventExpression
 from squidasm.run.stack.config import StackNetworkConfig
@@ -56,25 +53,16 @@ class ClientProgram(Program):
         conn = context.connection
         epr_socket = context.epr_sockets[self.PEER]
 
-        outcomes = conn.new_array(length=2)
-
-        def post_create(_: BaseNetQASMConnection, q: Qubit, index: RegFuture):
-            q.measure(future=outcomes.get_future_index(index))
-
-        epr_socket.create_keep(
-            number=2,
-            min_fidelity_all_at_end=70,
-            max_tries=20,
-            sequential=True,
-            post_routine=post_create,
+        eprs = epr_socket.create_keep(
+            number=2, min_fidelity_all_at_end=70, max_tries=20
         )
+
+        m0 = eprs[0].measure()
+        m1 = eprs[1].measure()
 
         yield from conn.flush()
 
-        m0 = int(outcomes.get_future_index(0))
-        m1 = int(outcomes.get_future_index(1))
-
-        return {"m0": m0, "m1": m1}
+        return {"m0": int(m0), "m1": int(m1)}
 
 
 class ServerProgram(Program):
@@ -95,25 +83,14 @@ class ServerProgram(Program):
         conn = context.connection
         epr_socket = context.epr_sockets[self.PEER]
 
-        outcomes = conn.new_array(length=2)
+        eprs = epr_socket.recv_keep(number=2, min_fidelity_all_at_end=70, max_tries=20)
 
-        def post_recv(_: BaseNetQASMConnection, q: Qubit, index: RegFuture):
-            q.measure(future=outcomes.get_future_index(index))
-
-        epr_socket.recv_keep(
-            number=2,
-            min_fidelity_all_at_end=70,
-            max_tries=20,
-            sequential=True,
-            post_routine=post_recv,
-        )
+        m0 = eprs[0].measure()
+        m1 = eprs[1].measure()
 
         yield from conn.flush()
 
-        m0 = int(outcomes.get_future_index(0))
-        m1 = int(outcomes.get_future_index(1))
-
-        return {"m0": m0, "m1": m1}
+        return {"m0": int(m0), "m1": int(m1)}
 
 
 PI = math.pi
